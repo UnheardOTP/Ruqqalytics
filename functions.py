48925f7ec5c33432f3565575e63669e14153b9c9
Unheard <unheard@unheard.org>
2020-09-21 10:20:14 -0400

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,29 @@
+import requests
+
+
+def refresh_api(client_id, client_secret, refresh_token):
+    # API Token Call
+    headers = {"User-Agent": "Data Analytics"}
+    url = "https://ruqqus.com/oauth/grant"
+    data = {"client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "refresh",
+        "refresh_token": refresh_token
+        }
+
+    r=requests.post(url, headers=headers, data=data)
+
+    token = r.json()['access_token']
+    return token
+
+def get_data(page_number, token):
+    # Authorization header
+    headers={"Authorization": "Bearer " + token,
+        "User-Agent": "Data Analytics"}
+
+    # Build URL
+    url=f"https://ruqqus.com/api/v1/front/listing?sort=new&page={page_number}"
+
+    # Call URL
+    r=requests.get(url, headers=headers)
+    return r.json()['data']
---
+++ script.py
@@ -0,0 +1,28 @@
+# Requirements
+from urllib.parse import urlparse
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime
+import config.config as config
+from functions import *
+
+# SQL Connection
+connection = mysql.connector.connect(host=config.host,
+                                    user=config.user,
+                                    password=config.password,
+                                    db=config.db)
+cursor = connection.cursor(prepared=True)
+
+# Loop over designated amount of pages
+for page_number in range(1, 2):
+    # Since tokens timeout every 10 minutes, go ahead and request a new token for each call
+    token = refresh_api(config.client_id, config.client_secret, config.refresh_token)
+    posts = get_data(page_number, token)
+    
+    for post in posts:
+        print(x['author'])
+
+cursor.close()
+connection.close()
+
+
