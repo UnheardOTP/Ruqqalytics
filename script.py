39da8c8df6b4c2f323ebad461192a44dd7c9a762
Unheard <unheard@unheard.org>
2020-09-22 12:41:29 -0400

    Removed data deletion, change time stamp method

--- script.py
+++ script.py
@@ -5,6 +5,10 @@ from mysql.connector import Error
 from datetime import datetime
 import config.config as config
 from functions import *
+from datetime import datetime
+
+# Generate timestamp for run time. Once the API allows searching posts by dates, this function can be removed.
+insert_date = datetime.now()
 
 # SQL Connection
 connection = mysql.connector.connect(host=config.host,
@@ -13,13 +17,13 @@ connection = mysql.connector.connect(host=config.host,
                                     db=config.db)
 cursor = connection.cursor(prepared=True)
 
-# Purge data from database
-sql = "DELETE FROM tblSiteContent"
-cursor.execute(sql)
+# Purge data from database -- Removing this as it will no longer be needed
+#sql = "DELETE FROM tblSiteContent"
+#cursor.execute(sql)
 
 # Loop over designated amount of pages
 
-for page_number in range(1, 2):
+for page_number in range(1, 2001):
     # Since tokens timeout every 10 minutes, go ahead and request a new token for each call
     token = refresh_api(config.client_id, config.client_secret, config.refresh_token)
     posts = get_data(page_number, token)
@@ -41,9 +45,10 @@ for page_number in range(1, 2):
             # Ensure the post is for a website and not for an internal ruqqus page
             if "http" in url:
                 sql = """INSERT INTO tblSiteContent 
-                        (url, title, source, score, submitter, submitted, guild, site) 
-                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
-                sql_values = (url,title,source,score,submitter,submitted,guild,"ruqqus")
+                        (url, title, source, score, submitter, submitted, guild, site, insert_date) 
+                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+                sql_values = (url,title,source,score,submitter,submitted,guild,"ruqqus", insert_date)
+                print(sql_values)
                 cursor.execute(sql, sql_values)
         except Exception as err:
             print(err)
